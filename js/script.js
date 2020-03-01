9c68dc49c5903c97eef9d3b8360efbe7b61566bd
davidzeck <edsodavid07@gmail.com>
2020-03-01 20:53:39 +0200

    new-jQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
 function openWin() {
   window.open("https://www.w3schools.com");
 }
+$(document).ready(function() {
+  $('.menu-item').hover(function(){
+  $('.image-caption',this).slideToggle('slow');
+}, function(){
+  $('.image-caption',this).slideToggle('slow');
+  });
+
+
+
+
+
+
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = $('.modalTrigger');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+$('.modalTrigger').on('click',function(){
+
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+
+});
+
+
+
