ed6a1109e733cca8a2addf511f82410ad54b34fc
davidzeck <edsodavid07@gmail.com>
2020-03-01 22:10:06 +0200

    new-js

--- js/script.js
+++ js/script.js
@@ -15,22 +15,19 @@ $(document).ready(function() {
 
 
 
-  // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = $('.modalTrigger');
 
-// Get the <span> element that closes the modal
+
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 $('.modalTrigger').on('click',function(){
 
   modal.style.display = "block";
 });
 
-// When the user clicks on <span> (x), close the modal
+// When user clicks  close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
@@ -44,7 +41,19 @@ window.onclick = function(event) {
 
 
 
+$( "#size" ).change(function() {
+  var number_pizza = $('#number_pizza').val();
+  var size = $('#size').val();
+  var total = number_pizza*size;
+  $('#total_amount').html(total);
+});
 
+$( "#number_pizza" ).keyup(function() {
+  var number_pizza = $('#number_pizza').val();
+  var size = $('#size').val();
+  var total = number_pizza*size;
+  $('#total_amount').html(total);
+});
 
 
 });
