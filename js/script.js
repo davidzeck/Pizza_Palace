4e91c97638c40525bc4eb775e30efb4a605e3a74
davidzeck <edsodavid07@gmail.com>
2020-03-01 15:35:42 +0200

    background-html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+function openWin() {
+  window.open("https://www.w3schools.com");
 }
